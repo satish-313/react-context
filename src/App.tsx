e6462e7dc237561c2daa9b446613da6aad7f7aff
satish-313 <pradhansatish53@gmail.com>
2022-11-21 18:15:18 +0530

    setup context, h1 don't work properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-function App() {
-  return (
-    <div>
-      <p className="text-3xl font-semibold">hello</p>
-    </div>
-  )
+import { useState, useEffect, createContext, useContext } from "react";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defence: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
 }
 
-export default App
+const Theme = createContext("");
+
+function usePokemon(): { pokemon: Pokemon[] } {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("/pokemon.json")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }, []);
+  return { pokemon };
+}
+
+const PokemonList = ({ pokemon }: { pokemon: Pokemon[] }) => {
+  const theme = useContext(Theme);
+  return (
+    <div>
+      <div>
+        <h1>Theme : {theme}</h1>
+      </div>
+      {pokemon.map((p) => (
+        <div key={p.id}>{p.name}</div>
+      ))}
+    </div>
+  );
+};
+
+
+
+function App() {
+  const { pokemon } = usePokemon();
+  return (
+    <Theme.Provider value="light">
+      <PokemonList pokemon={pokemon} />{" "}
+    </Theme.Provider>
+  );
+}
+
+export default App;
