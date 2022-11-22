fd619f511278a49cb4c92f1e6123bb871fd31fdd
satish-313 <pradhansatish53@gmail.com>
2022-11-22 12:08:30 +0530

    build native state management

--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,12 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+  useCallback,
+  useMemo,
+} from "react";
 
 interface Pokemon {
   id: number;
@@ -12,20 +20,68 @@ interface Pokemon {
   speed: number;
 }
 
- const PokemonContext = createContext<
-  ReturnType<typeof usePokemonSource>
->({} as unknown as ReturnType<typeof usePokemonSource>);
+const PokemonContext = createContext<ReturnType<typeof usePokemonSource>>(
+  {} as unknown as ReturnType<typeof usePokemonSource>
+);
 
- function usePokemonSource(): { pokemon: Pokemon[] } {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+function usePokemonSource(): {
+  pokemon: Pokemon[];
+  search: string;
+  setSearch: (search: string) => void;
+} {
+  // const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  // const [search, setSearch] = useState("");
+  type PokemonState = {
+    pokemon: Pokemon[];
+    search: string;
+  };
+  type PokemonAction =
+    | {
+        type: "setPokemon";
+        payload: Pokemon[];
+      }
+    | { type: "setSearch"; payload: string };
+  const [{ pokemon, search }, dispatch] = useReducer(
+    (state: PokemonState, action: PokemonAction) => {
+      switch (action.type) {
+        case "setPokemon":
+          return { ...state, pokemon: action.payload };
+        case "setSearch":
+          return { ...state, search: action.payload };
+      }
+    },
+    { pokemon: [], search: "" }
+  );
 
   useEffect(() => {
     fetch("/pokemon.json")
       .then((res) => res.json())
-      .then((data) => setPokemon(data));
+      .then((data) => dispatch({ type: "setPokemon", payload: data }));
   }, []);
 
-  return { pokemon };
+  const setSearch = useCallback((search: string) => {
+    dispatch({
+      type: "setSearch",
+      payload: search,
+    });
+  }, []);
+
+  const filterPokemon = useMemo(
+    () =>
+      pokemon
+        .filter((p) =>
+          p.name.toLowerCase().includes(search.toLocaleLowerCase())
+        )
+        .slice(0, 20),
+    [pokemon, search]
+  );
+
+  const sortedPokemon = useMemo(
+    () => [...filterPokemon].sort((a, b) => a.name.localeCompare(b.name)),
+    [filterPokemon]
+  );
+
+  return { pokemon: sortedPokemon, search, setSearch };
 }
 
 export function usePokemon() {
